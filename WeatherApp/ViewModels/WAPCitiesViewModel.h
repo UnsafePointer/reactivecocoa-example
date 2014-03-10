ade819633da974cb1aaeccb6171941d28037de34
Renzo Crisostomo <ruenzuo@gmail.com>
2014-03-10 11:23:25 +0100

    Adding ViewModels

---
+++ WeatherApp/ViewModels/WAPCitiesViewModel.h
@@ -0,0 +1,13 @@
+//
+//  WAPCitiesViewModel.h
+//  WeatherApp
+//
+//  Created by Renzo Crisostomo on 10/03/14.
+//  Copyright (c) 2014 Renzo Crisóstomo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WAPCitiesViewModel : NSObject
+
+@end
---
+++ WeatherApp/ViewModels/WAPCitiesViewModel.m
@@ -0,0 +1,13 @@
+//
+//  WAPCitiesViewModel.m
+//  WeatherApp
+//
+//  Created by Renzo Crisostomo on 10/03/14.
+//  Copyright (c) 2014 Renzo Crisóstomo. All rights reserved.
+//
+
+#import "WAPCitiesViewModel.h"
+
+@implementation WAPCitiesViewModel
+
+@end
